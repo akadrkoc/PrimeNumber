af49a7b8c0f411024dc1359b21ed9ab0cceea450
Abdulkadir Koc <abdulkadir1308@gmail.com>
2025-06-16 12:12:37 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+    public static boolean isPrime(int number) {
+        if (number < 2)
+            return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }return true;
+    }
+}
